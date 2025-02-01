afb1a5956978f51d059daae18d5f7d5af074605e
nurhossainfarid <faahsan1516@gmail.com>
2025-02-01 18:59:40 +0600

    Start implement all products

--- src/pages/bicycle/AllBicycle.tsx
+++ src/pages/bicycle/AllBicycle.tsx
@@ -1,9 +1,17 @@
-import Title from "@/components/ui/title";
+import ProductCard from "@/components/products/ProductCard";
+import { bicyclesData } from "@/constants";
 
 const AllBicycle = () => {
   return (
-    <div>
-      <Title text="All Bicycle" fontSize="xl" />
+    <div className="flex flex-col items-center gap-10 pt-10">
+      <h1 className="font-outfit text-xl md:text-2xl lg:text-4.5xl uppercase font-semibold text-center">
+        All Products
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10">
+        {bicyclesData.map((bicycle) => (
+          <ProductCard key={bicycle.name} bicycle={bicycle} />
+        ))}
+      </div>
     </div>
   );
 };
